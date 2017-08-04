0b293a3beab8b314f269f53da2bd707e09db5d4e
johnSZanoni <john@zanoni.nl>
2017-08-04 15:40:59 +0200

    Update Demo.vue

--- components/Demo.vue
+++ components/Demo.vue
@@ -1,1 +1,33 @@
-//demo
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'demo'
+  data () {
+    return {
+      msg: 'Demo App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
